b7d48c27cee218f99b0017fc30a790f0f67092a6
bmartinko <bennett.martinko@gmail.com>
2018-10-12 02:06:28 -0400

    I finally finished this. It turns out that I needed to rework some stuff that I had, and that took a bit of time to do. Other than that, I had most of the work done before even working on it tonight.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,20 +5,27 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner kbsreader = new Scanner(System.in);
-        System.out.println("Please enter your name here:");
-        String name = kbsreader.nextLine();
 
-        perfectRandom personOne = new perfectRandom();
-        System.out.println("Nice to meet you " + name + "! Your lucky number is: " + personOne);
-        System.out.println("Press 0 to exit, 1 to see the name and object again, and 2 to enter another name");
-        int press = kbsreader.nextInt();
-        if (press == 0){
-            break;
-        }else if (press == 1){
-            System.out.println("Nice to meet you " + name + "! Your lucky number is: " + personOne);
-        }else (press == 2){
+        int press = 3;
+
+        while(true){
+        if (press == 2 || press == 3){
             System.out.println("Please enter your name here:");
             String name = kbsreader.nextLine();
         }
-}
+        String name = kbsreader.nextLine();
+
+        perfectRandom personOne = new perfectRandom((int)(Math.random() * 15));
+
+        System.out.println("Press 0 to end, Press 1 to receive your lucky number, Press 2 to enter another name.");
+
+         press = kbsreader.nextInt();
+        if (press == 0){
+            break;
+        }else if (press == 1){
+            System.out.println("Nice to meet you " + name + "! Your lucky number is: " + personOne.value);
+            }
+
+         }
+    }
 }
--- src/com/company/perfectRandom.java
+++ src/com/company/perfectRandom.java
@@ -4,15 +4,16 @@ package com.company;
  * Created by bm846 on 10/9/18.
  */
 public class perfectRandom {
-    public int random;
+    public int number;
+    public int value;
 
-
-    public int perfectRandom(int r){
-        random = (int)(Math.random()*10);
-        r = random * random;
-       return r;
+    public perfectRandom(int n){
+        n = (int)(Math.random() * 10);
+        number = n;
+        value = number * number;
 
     }
+
 
 
 }
